9152ccf067260461949566675e3e7e6d05eeecf0
Eric S Weilnau <eweilnau@brightree.com>
2021-12-03 20:38:48 -0500

    Add unit tests for day_01

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,44 +1,80 @@
 pub fn print_answers() {
     println!("\n--- Day 1: Sonar Sweep ---");
-    let depth_measurements = sonar_sweep();
+    let sonar = Sonar::new(&std::fs::read_to_string("assets\\day_1_input.txt").unwrap());
     println!(
         "How many measurements are larger than the previous measurement? {}",
-        count_depth_measurement_increases(&depth_measurements)
+        sonar.count_depth_measurement_increases(),
     );
     println!(
         "Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum? {}",
-        count_depth_sliding_sum_increases(&depth_measurements, 3)
+        sonar.count_depth_sliding_sum_increases(3),
     );
 }
 
-fn sonar_sweep() -> Vec<i32> {
-    std::fs::read_to_string("assets\\day_1_input.txt")
-        .unwrap()
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect()
+struct Sonar {
+    depth_measurements: Vec<u16>,
 }
 
-fn count_depth_measurement_increases(data: &[i32]) -> i32 {
-    let mut results = 0;
-    for i in 1..data.len() {
-        if data[i - 1] < data[i] {
-            results += 1;
-        }
+impl Sonar {
+    fn new(input: &str) -> Self {
+        let depth_measurements = input
+            .lines()
+            .map(|line| line.parse::<u16>().unwrap())
+            .collect::<Vec<_>>();
+        Self { depth_measurements }
     }
-    results
+
+    fn count_depth_measurement_increases(&self) -> usize {
+        let mut results = 0;
+        for i in 1..self.depth_measurements.len() {
+            if self.depth_measurements[i - 1] < self.depth_measurements[i] {
+                results += 1;
+            }
+        }
+        results
+    }
+
+    fn count_depth_sliding_sum_increases(&self, sliding_window_size: usize) -> usize {
+        let mut results = 0;
+        for i in 1..self.depth_measurements.len() - (sliding_window_size - 1) {
+            let previous_sum = self.depth_measurements[i - 1..i + (sliding_window_size - 1)]
+                .iter()
+                .sum::<u16>();
+            let current_sum = self.depth_measurements[i..i + sliding_window_size]
+                .iter()
+                .sum::<u16>();
+            if previous_sum < current_sum {
+                results += 1
+            }
+        }
+        results
+    }
 }
 
-fn count_depth_sliding_sum_increases(data: &[i32], sliding_window_size: usize) -> i32 {
-    let mut results = 0;
-    for i in 1..data.len() - (sliding_window_size - 1) {
-        let previous_sum = data[i - 1..i + (sliding_window_size - 1)]
-            .iter()
-            .sum::<i32>();
-        let current_sum = data[i..i + sliding_window_size].iter().sum::<i32>();
-        if previous_sum < current_sum {
-            results += 1
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+    #[test]
+    fn test_count_depth_measurement_increases() {
+        let sonar = Sonar::new(INPUT);
+        assert_eq!(sonar.count_depth_measurement_increases(), 7);
     }
-    results
+
+    #[test]
+    fn test_count_depth_sliding_sum_increases() {
+        let sonar = Sonar::new(INPUT);
+        assert_eq!(sonar.count_depth_sliding_sum_increases(3), 5);
+    }
 }
